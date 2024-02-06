47912123ceeb68eba3d72ae11a9442fb800e9b41
Peter Sisk <thetechwizard42@gmail.com>
2024-02-06 09:46:43 -0800

    linted the code

--- quanta.py
+++ quanta.py
@@ -1,42 +1,57 @@
+"""
+    Demo script adapted from this article
+    https://medium.datadriveninvestor.com/stock-price-prediction-with-quantum-
+        machine-learning-in-python-54948a3da389
+    to compare classical and quantum machine learning algorithms
+"""
+import multiprocessing
+import requests
 import numpy as np
 import pandas as pd
-import requests
-import json
-import multiprocessing
-import tensorflow as tf
-from sklearn.model_selection import train_test_split
-from tensorflow.keras.models import Sequential
-from tensorflow.keras.layers import Dense
+from keras.models import Sequential
+from keras.layers import Dense
 from sklearn.metrics import mean_squared_error
+import  matplotlib.pyplot as plt
 from qiskit import QuantumCircuit
 from qiskit.circuit.library import PauliFeatureMap
-from qiskit_algorithms.optimizers import ADAM
 from qiskit.circuit import Parameter
-from qiskit.primitives import Sampler
 from qiskit_machine_learning.algorithms import VQR
-import matplotlib.pyplot as plt
+from qiskit_algorithms.optimizers import ADAM
 
-def run_main(ticker):
-    # First, we fetch the data using the historical Data API endpoint provided by Financial Modeling Prep as follows:
+API_KEY = "ebb4b855e1bdd6c131ea21dc938988eb"
+BASE_URL = "https://financialmodelingprep.com/api/v3/historical-price-full"
 
-    API_KEY = "ebb4b855e1bdd6c131ea21dc938988eb"
+"""
+    The main entry point to the program. Gets data, plots it in a separate
+    process, then classifies it first using a classical algorithm and then
+    using a quantum algorithm
+"""
+def run_main(tkr):
+    """
+    The main entry point to the program. Gets data, plots it in a separate
+    process, then classifies it first using a classical algorithm and then
+    using a quantum algorithm
+    """
 
-    api_url = f"https://financialmodelingprep.com/api/v3/historical-price-full/{ticker}?apikey={API_KEY}"
+    # First, we fetch the data using the historical Data API endpoint provided
+    # by Financial Modeling Prep as follows:
+
+    api_url = f"https://{BASE_URL}/{tkr}?apikey={API_KEY}"
 
     df = get_data(api_url)
     (x_train, y_train, x_test, y_test) = process_data(df)
-    # Need to display the data plot in a separate thread or process so that execution can continue
-    # without dismissing the data plot.
-    # Use the multiprocessing module to perform the plotting activity in another process (i.e., on another core)
-    # Can't use threads because matplotlib is not thread-safe.
-    plotting_job = multiprocessing.Process(target=plot_data, kwargs={"df":df, "ticker":ticker})
+
+    plotting_job = multiprocessing.Process(target=plot_data, kwargs={"df":df, "tkr":tkr})
     plotting_job.start()
     classical_classifier(x_train, y_train, x_test, y_test)
     quantum_classifier(x_train, y_train, x_test, y_test)
 
 def get_data(api_url):
+    """
+        Retrieves stock data from the URL
+    """
     # Make a GET request to the API
-    response = requests.get(api_url)
+    response = requests.get(api_url, timeout=5)
 
     # Check if the request was successful (status code 200)
     if response.status_code == 200:
@@ -49,19 +64,21 @@ def get_data(api_url):
     return df
 
 def process_data(df):
-    # From this plethora of data, we are going to use open price as our temporal variable and we will work 
-    # with 500 data points each representing daily open prices, and our window size for prediction would be 2.
+    """
+    From this plethora of data, we are going to use open price as our temporal 
+    variable and we will work with 500 data points each representing daily open 
+    prices, and our window size for prediction would be 2.
+    """
     df.tail()
     final_data = df[['open', 'date']][0:500] #forming filtered dataframe
     input_sequences = []
     labels = []
-
     #Creating input and output data for time series forecasting
     for i in range(len(final_data['open'])):
         if i > 1:
             labels.append(final_data['open'][i])
             input_sequences.append(final_data['open'][i-1:i+1].tolist())
-      
+
     #creating train test split
     x_train = np.array(input_sequences[0:400])
     x_test = np.array(input_sequences[400:])
@@ -69,7 +86,11 @@ def process_data(df):
     y_test = np.array(labels[400:])
     return (x_train, y_train, x_test, y_test)
 
-def plot_data(df, ticker):
+def plot_data(df, tkr):
+    """
+        Displas a plot of the data. Runs in a separate process
+        in order not to delay the execution of the remainder of the code.
+    """
     plt.style.use('ggplot')
 
     # Convert the 'date' column to datetime format
@@ -82,14 +103,16 @@ def plot_data(df, ticker):
     # Adding labels and title
     plt.xlabel('date')
     plt.ylabel('open')
-    plt.title(f"Time Series Data for {ticker}")
+    plt.title(f"Time Series Data for {tkr}")
 
     # Display the plot
     plt.grid(True)
     plt.show()
 
 def quantum_classifier(x_train, y_train, x_test, y_test):
-    #Now comes the Quantum stuff
+    """
+        Classifies the trained data using Qiskit's VQR classifier
+    """
     num_features =  2
     feature_map = PauliFeatureMap(feature_dimension = num_features, reps = 2)
     optimizer = ADAM(maxiter = 100)
@@ -125,7 +148,10 @@ def quantum_classifier(x_train, y_train, x_test, y_test):
     vqr_rmse = np.sqrt(vqr_mse)
     print(f"Quantum root mean squared error: {vqr_rmse}")
 
-def classical_classifier(x_train, y_train, x_test, y_test):    
+def classical_classifier(x_train, y_train, x_test, y_test):
+    """
+        Classifies the data using TensorFlow's Sequential model
+    """
     model = Sequential()
     model.add(Dense(64,activation = 'relu', input_shape = (2,)))
     model.add(Dense(1))
@@ -134,7 +160,6 @@ def classical_classifier(x_train, y_train, x_test, y_test):
 
     model.fit(x_train, y_train, epochs = 20, batch_size = 32, validation_data = (x_test,y_test))
 
-    loss = model.evaluate(x_test, y_test)
     prediction = model.predict(x_test)
     flattened = prediction.flatten()
     ann_mse = mean_squared_error(y_test, flattened)
